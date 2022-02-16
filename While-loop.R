30434e6c0b30d0b84dd3ee4b8ea8e6b4839cc715
Bibek Mainali <bibek.mainali20@gmail.com>
2022-02-16 09:18:37 -0500

    Logical Operators in R

---
+++ While-loop.R
@@ -0,0 +1,14 @@
+# constructing while loop
+# while(logical expression){
+  #body of the loop}
+
+while(FALSE){
+  print('Hello')
+}
+
+# lets create a counter loop
+counter <- 1
+while(counter < 12){
+  print(counter)
+  counter <- counter +1
+}
